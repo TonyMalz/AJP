7a0c3973af21e8d655cd49336f69f084ab29891d
Christian Zeck <christian-konrad.zeck@stud.uni-bamberg.de>
2014-04-30 14:51:27 +0200

    updated OutputFormatter + Test for new ScanResult

--- Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/search/impl/OutputFormatterImpl.java
+++ Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/search/impl/OutputFormatterImpl.java
@@ -8,15 +8,12 @@ import java.nio.file.Path;
 import java.util.List;
 
 import de.uniba.wiai.dsg.ajp.assignment1.search.OutputFormatter;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.IScanResult;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultFound;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultNoPath;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultNotFound;
+import de.uniba.wiai.dsg.ajp.assignment1.search.ScanResult;
 
 public class OutputFormatterImpl implements OutputFormatter {
 
     @Override
-    public void show(Path resultPath, List<IScanResult> results)
+    public void show(Path resultPath, List<ScanResult> results)
 	    throws IOException {
 
 	// path must not be null
@@ -34,83 +31,78 @@ public class OutputFormatterImpl implements OutputFormatter {
 	}
 
 	try (BufferedWriter writer = Files.newBufferedWriter(resultPath,
-		StandardCharsets.UTF_8)) {
+	        StandardCharsets.UTF_8)) {
 	    // if list "results" has no results, just "ScanResultNoPath"
-	    if (results.get(0) instanceof ScanResultNoPath) {
-		writer.write("The project includes "
-			+ ((ScanResultNoPath) results.get(0)).token
-			+ " 0 time(s)");
-	    }
+	    // if (results.get(0) instanceof ScanResultNoPath) {
+	    // writer.write("The project includes "
+	    // + ((ScanResultNoPath) results.get(0)).token
+	    // + " 0 time(s)");
+	    // }
 	    // else "results" isn't empty
-	    else {
-		// counter for iterating list "results"
-		// projectFound for counting results
-		int counter = 0;
-		int projectFound = 0;
-		String currentFile;
-		String token;
-		if (results.get(0) instanceof ScanResultFound) {
-		    token = ((ScanResultFound) results.get(0)).token;
-		} else {
-		    token = ((ScanResultNotFound) results.get(0)).token;
-		}
+	    // else {
+	    // counter for iterating list "results"
+	    // projectFound for counting results
+	    int counter = 0;
+	    int projectFound = 0;
+	    Path currentFile;
+	    String token;
 
-		while (counter < results.size()) {
-		    // if entry is instance of "ScanResultNotFound" there is no
-		    // searchResult in this file, so print file summary
-		    if (results.get(counter) instanceof ScanResultNotFound) {
-			writer.write(((ScanResultNotFound) results.get(counter)).fileName
-				+ " includes "
-				+ ((ScanResultNotFound) results.get(counter)).token
-				+ " " + " 0 time(s)");
-			writer.newLine();
+	    token = results.get(0).token;
+
+	    while (counter < results.size()) {
+		// if entry is instance of "ScanResultNotFound" there is no
+		// searchResult in this file, so print file summary
+
+		if (results.get(counter).lineContent == null
+		        && results.get(counter).column == 0
+		        && results.get(counter).lineNumber == 0) {
+		    writer.write(results.get(counter).fileName + " includes "
+			    + results.get(counter).token + " " + " 0 time(s)");
+		    writer.newLine();
+		    writer.newLine();
+		    counter++;
+		} else {
+
+		    // save fileName as currentFile
+		    currentFile = results.get(counter).fileName;
+		    int fileFound = 0;
+
+		    // while not all results are processed and entry is a
+		    // real found result and fileNames equal currentFile,
+		    // write result-line
+		    // -> means, write all lines for the whole file
+		    while (counter < results.size()
+
+		    && results.get(counter).fileName.equals(currentFile)) {
+			ScanResult scanElement = results.get(counter);
+			writer.write(scanElement.fileName + ":");
+			writer.write(scanElement.lineNumber + ",");
+			writer.write(scanElement.column + " > ");
+			writer.write(scanElement.lineContent.substring(0,
+			        scanElement.column));
+			writer.write("**" + scanElement.token + "**");
+			writer.write(scanElement.lineContent
+			        .substring((scanElement.column)
+			                + scanElement.token.length()));
 			writer.newLine();
 			counter++;
-		    } else {
-
-			// save fileName as currentFile
-			currentFile = ((ScanResultFound) results.get(counter)).fileName;
-			int fileFound = 0;
-
-			// while not all results are processed and entry is a
-			// real found result and fileNames equal currentFile,
-			// write result-line
-			// -> means, write all lines for the whole file
-			while (counter < results.size()
-				&& results.get(counter) instanceof ScanResultFound
-				&& ((ScanResultFound) results.get(counter)).fileName
-					.equals(currentFile)) {
-			    ScanResultFound scanElement = (ScanResultFound) results
-				    .get(counter);
-			    writer.write(scanElement.fileName + ":");
-			    writer.write(scanElement.lineNumber + ",");
-			    writer.write(scanElement.column + " > ");
-			    writer.write(scanElement.lineContent.substring(0,
-				    scanElement.column));
-			    writer.write("**" + scanElement.token + "**");
-			    writer.write(scanElement.lineContent
-				    .substring((scanElement.column)
-					    + scanElement.token.length()));
-			    writer.newLine();
-			    counter++;
-			    fileFound++;
-			}
-			// file finished -> file summary
-			writer.write(currentFile + " includes " + token + " "
-				+ fileFound + " time(s)");
-			writer.newLine();
-			writer.newLine();
-			projectFound = projectFound + fileFound;
+			fileFound++;
 		    }
-
+		    // file finished -> file summary
+		    writer.write(currentFile + " includes " + token + " "
+			    + fileFound + " time(s)");
+		    writer.newLine();
+		    writer.newLine();
+		    projectFound = projectFound + fileFound;
 		}
-
-		// project finished -> project summary
-		writer.write("The project includes " + token + " "
-			+ projectFound + " time(s)");
 
 	    }
 
+	    // project finished -> project summary
+	    writer.write("The project includes " + token + " " + projectFound
+		    + " time(s)");
+
 	}
+
     }
 }
--- Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/test/OutputFormatterTest.java
+++ Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/test/OutputFormatterTest.java
@@ -5,11 +5,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.uniba.wiai.dsg.ajp.assignment1.search.ScanResult;
 import de.uniba.wiai.dsg.ajp.assignment1.search.impl.OutputFormatterImpl;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.IScanResult;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultFound;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultNoPath;
-import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultNotFound;
 
 /**
  * Class for testing OutputFormatter
@@ -21,43 +18,24 @@ import de.uniba.wiai.dsg.ajp.assignment1.search.impl.result.ScanResultNotFound;
 public class OutputFormatterTest {
 
     public static void main(String[] args) throws IOException {
-	ScanResultFound sr1 = new ScanResultFound();
-	sr1.fileName = "data/other.txt";
-	sr1.token = "simple";
-	sr1.lineContent = "simple";
-	sr1.lineNumber = 2;
-	sr1.column = 0;
+	ScanResult sr1 = new ScanResult(Paths.get("data/other.txt"), "simple",
+	        "simple", 2, 0);
 
-	ScanResultFound sr2 = new ScanResultFound();
-	sr2.fileName = "data/test.txt";
-	sr2.token = "simple";
-	sr2.lineContent = "very simple and extremely simple";
-	sr2.lineNumber = 2;
-	sr2.column = 5;
+	ScanResult sr2 = new ScanResult(Paths.get("data/test.txt"), "simple",
+	        "very simple and extremely simple", 2, 5);
 
-	ScanResultFound sr3 = new ScanResultFound();
-	sr3.fileName = "data/test.txt";
-	sr3.token = "simple";
-	sr3.lineContent = "very simple and extremely simple";
-	sr3.lineNumber = 2;
-	sr3.column = 26;
+	ScanResult sr3 = new ScanResult(Paths.get("data/test.txt"), "simple",
+	        "very simple and extremely simple", 2, 26);
 
-	ScanResultNotFound srnf1 = new ScanResultNotFound();
-	srnf1.fileName = "data/nothing.txt";
-	srnf1.token = "simple";
+	ScanResult sr4 = new ScanResult(Paths.get("data/nothing.txt"), "simple");
 
-	ScanResultNotFound srnf2 = new ScanResultNotFound();
-	srnf2.fileName = "data/not.txt";
-	srnf2.token = "simple";
+	ScanResult sr5 = new ScanResult(Paths.get("data/not.txt"), "simple");
 
-	ScanResultNoPath srnp1 = new ScanResultNoPath();
-	srnp1.token = "simple";
-
-	List<IScanResult> results = new ArrayList<IScanResult>();
+	List<ScanResult> results = new ArrayList<ScanResult>();
 	// results.add(srnp1);
-	results.add(srnf2);
+	results.add(sr5);
 	results.add(sr1);
-	results.add(srnf1);
+	results.add(sr4);
 	results.add(sr2);
 	results.add(sr3);
 
