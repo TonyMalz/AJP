fa672a653510bfabf7d78b420f4e132120f12a7d
Tony <tony-serjoscha.malzhacker@stud.uni-bamberg.de>
2014-05-06 20:50:17 +0200

    restore original interface

--- Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/search/TokenFinder.java
+++ Assignment1/src/de/uniba/wiai/dsg/ajp/assignment1/search/TokenFinder.java
@@ -1,12 +1,11 @@
 package de.uniba.wiai.dsg.ajp.assignment1.search;
 
-import java.io.IOException;
 
 /**
  * The interface to be implemented
  */
 public interface TokenFinder {
 
-	void search(SearchTask task) throws TokenFinderException, IOException;
+	void search(SearchTask task) throws TokenFinderException;
 
 }
