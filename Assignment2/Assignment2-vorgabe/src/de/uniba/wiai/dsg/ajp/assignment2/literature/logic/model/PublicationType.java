810823323f218e44d67b7c7eb53fceab3bed9abd
Mathias Müller <Mathias-kurt.mueller@stud.uni-bamberg.de>
2014-05-07 13:59:07 +0200

    deleted files with bugs

--- Assignment2/Assignment2-vorgabe/src/de/uniba/wiai/dsg/ajp/assignment2/literature/logic/model/PublicationType.java
+++
@@ -1,8 +0,0 @@
-package de.uniba.wiai.dsg.ajp.assignment2.literature.logic.model;
-
-
-public enum PublicationType {
-
-	ARTICLE, TECHREP, BOOK, MASTERSTHESIS, PHDTHESIS, INPROCEEDINGS
-
-}
--- Assignment2/Assignment2/src/de/uniba/wiai/dsg/ajp/assignment2/literature/Main.java
+++
@@ -1,9 +0,0 @@
-package de.uniba.wiai.dsg.ajp.assignment2.literature;
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO startet eure Anwendung ueber diese main-Methode
-	}
-
-}
