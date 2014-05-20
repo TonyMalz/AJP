446290947e0b2389db52beadb3c6d5cfd9018ed6
Mathias Mueller <mathias-kurt.mueller@stud.uni-bamberg.de>
2014-05-20 08:49:04 +0200

    Added files for Assignement 3.

---
+++ Assignment2/src/de/uniba/wiai/dsg/ajp/assignment2/literature/logic/action/ListAuthorAction.java
@@ -0,0 +1,39 @@
+package de.uniba.wiai.dsg.ajp.assignment2.literature.logic.action;
+
+import java.io.IOException;
+
+import de.uniba.wiai.dsg.ajp.assignment2.literature.Main.DatabaseRequest;
+import de.uniba.wiai.dsg.ajp.assignment2.literature.logic.DatabaseAction;
+
+public class ListAuthorAction extends DatabaseAction {
+
+	public ListAuthorAction(DatabaseRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void readInput() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void validateInput() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void process() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
