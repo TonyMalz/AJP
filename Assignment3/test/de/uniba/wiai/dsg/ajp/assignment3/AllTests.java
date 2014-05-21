e85c9bf6e36ac5a263619996699945b58538c69b
Mathias Mueller <mathias-kurt.mueller@stud.uni-bamberg.de>
2014-05-21 14:47:22 +0200

    Added test sutie for JUnit tests.

---
+++ Assignment3/test/de/uniba/wiai/dsg/ajp/assignment3/AllTests.java
@@ -0,0 +1,13 @@
+package de.uniba.wiai.dsg.ajp.assignment3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChidrensPriceTest.class, CustomerTest.class, MovieTest.class,
+	NewReleasePriceTest.class, PriceTest.class, RegularPriceTest.class,
+	RentalTest.class })
+public class AllTests {
+
+}
