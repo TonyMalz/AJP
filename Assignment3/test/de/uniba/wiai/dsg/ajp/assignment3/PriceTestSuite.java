fe08588c16935c0e3e33299eabb8d2518c3f21cd
Mathias Mueller <mathias-kurt.mueller@stud.uni-bamberg.de>
2014-05-26 11:09:06 +0200

    Added a test suite for prices.

---
+++ Assignment3/test/de/uniba/wiai/dsg/ajp/assignment3/PriceTestSuite.java
@@ -0,0 +1,12 @@
+package de.uniba.wiai.dsg.ajp.assignment3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChidrensPriceTest.class, NewReleasePriceTest.class,
+	RegularPriceTest.class })
+public class PriceTestSuite {
+
+}
